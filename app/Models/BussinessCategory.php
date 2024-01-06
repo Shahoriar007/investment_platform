f66aed84fc857f546008afc6db120a5b55b77678
Samia-Sultana <samia.cse.ewu@gmail.com>
2024-01-07 00:00:27 +0600

    add model and controller

---
+++ app/Models/BussinessCategory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+
+
+use App\Traits\CreatedBy;
+use App\Traits\DeletedBy;
+use App\Traits\UpdatedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BussinessCategory extends Model
+{
+    use HasFactory;
+    use CreatedBy;
+    use UpdatedBy;
+    use DeletedBy;
+
+    protected $fillable = [
+        'name',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by', 'id');
+    }
+
+}
